929d3e2c37020b896e8dbed1775fbe5a2a21d324
lun <116100299+qiujinglun@users.noreply.github.com>
2026-05-11 14:26:20 +0800

    week12 practice

--- REACT exercises/05_resultcard_two_cards/starter/src/App.jsx
+++ REACT exercises/05_resultcard_two_cards/starter/src/App.jsx
@@ -1,17 +1,48 @@
 function ResultCard(props) {
   return (
     <div className="card">
-      <h3>請改成 props.title</h3>
-      <p>請改成 props.desc</p>
+      <h3>{props.title}</h3>
+      <p>{props.desc}</p>
     </div>
   );
 }
+function ResultCard2({ title, rating, desc, isSale, tags }) {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>評分：{rating}</p>
+      <p>{desc}</p>
+      {isSale && <span>特價中</span>}
+      <p>標籤：{tags.join("、")}</p>
+    </div>
+  );
+}
+
 
 export default function App() {
   return (
     <div className="container">
       <h1>請完成兩張 ResultCard</h1>
-      <ResultCard />
+      <ResultCard title="React 基礎" desc="學習 component、JSX、props" />
+      <ResultCard title="React 進階" desc="學習 state、event、hook" />
+      <ResultCard2 title="React 基礎" rating={5} desc="學習 component、JSX、props" isSale={true} tags={["前端", "JavaScript", "元件化"]} />
+      <ResultCard2 title="網站教學" rating={4} desc="學習前端開發基礎" isSale={false} tags={["前端", "後端", "API"]} />
     </div>
   );
 }
+
+//APP
+// --Title
+// --SearchForm
+// ----SearchInput
+// --ResultList
+// ----ResultCard
+// ------CardTitle
+// ------CardDesc
+// ------CardTags
+// ----ResultCard2
+// ------CardTitle
+// ------CardRating
+// ------CardDesc
+// ------CardSale
+// ------CardTags
--- REACT exercises/06_resultcard_destructured/starter/src/App.jsx
+++ REACT exercises/06_resultcard_destructured/starter/src/App.jsx
@@ -1,8 +1,43 @@
-function ResultCard(props) {
+// function ResultCard(props) {
+//   return (
+//     <div className="card">
+//       <h3>{props.title}</h3>
+//       <p>{props.desc}</p>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   return (
+//     <div className="container">
+//       <h1>請把 ResultCard 改成解構版</h1>
+//       <ResultCard title="React 基礎" desc="學習 component、JSX、props" />
+//     </div>
+//   );
+// }
+
+function SearchForm() {
+  return (
+    <div className="search-box">
+      <input type="text" placeholder="請輸入關鍵字" />
+      <button>搜尋</button>
+    </div>
+  );
+}
+
+function ResultCard({ title, desc }) {
   return (
     <div className="card">
-      <h3>{props.title}</h3>
-      <p>{props.desc}</p>
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </div>
+  );
+}
+function ResultList() {
+  return (
+    <div>
+      <ResultCard title="React 課程" desc="認識元件化網頁前端" />
+      <ResultCard title="網站建置管理" desc="了解網頁前後端相關知識" />
     </div>
   );
 }
@@ -10,8 +45,9 @@ function ResultCard(props) {
 export default function App() {
   return (
     <div className="container">
-      <h1>請把 ResultCard 改成解構版</h1>
-      <ResultCard title="React 基礎" desc="學習 component、JSX、props" />
+      <h1>課程查詢工具</h1>
+      <SearchForm />
+      <ResultList />
     </div>
   );
 }
